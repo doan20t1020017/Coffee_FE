2612e4937e97be6d05f9c064bd944dc28ca1ab9d
Nhan Bui <buithiennhan0345@gmail.com>
2024-05-12 09:41:46 +0700

    handle add product into cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
+import { useContext } from "react";
+import { StorageContext } from "../../Contexts/StorageContext";
 
 const fakeCart = [];
 
 function Cart() {
+  const storage = useContext(StorageContext);
   const cart = fakeCart;
 
   return (
@@ -15,7 +18,7 @@ function Cart() {
       <h2 className="mt-7 text-xl font-semibold">Giỏ Hàng Của Bạn</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
-        {cart.map((item) => (
+        {storage.cartItems.map((item) => (
           <CartItem item={item} key={item.key} />
         ))}
       </ul>
--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,8 +1,10 @@
-import Button from '../../ui/Button';
-import { formatCurrency } from '../../utils/helpers';
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
 
-function CartItem({ item }) {
-  const { id, name, quantity, totalPrice } = item;
+function CartItem({
+  item = { id: 1, name: "Bạc xỉu", quantity: 2, totalPrice: 200 },
+}) {
+  const { name, quantity, unitPrice } = item;
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -10,7 +12,9 @@ function CartItem({ item }) {
         {quantity}&times; {name}
       </p>
       <div className="flex items-center justify-between sm:gap-6">
-        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <p className="text-sm font-bold">
+          {formatCurrency(quantity * unitPrice)}
+        </p>
         <Button type="small">Xóa</Button>
       </div>
     </li>
